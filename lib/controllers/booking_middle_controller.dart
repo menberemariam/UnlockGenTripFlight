69fa7937b36c5a006a1221e008e52bd89cc421f9
Menberemariyam <menberemariam123@email.com>
2026-02-13 11:43:37 +0300

    Add my controller and screen for trip app

---
+++ lib/controllers/booking_middle_controller.dart
@@ -0,0 +1,28 @@
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+class HomeController extends GetxController {
+
+  final isExpanded = false.obs;
+
+  void toggleExpanded() => isExpanded.value = !isExpanded.value;
+
+  final List<Map<String, String>> cheapFlights = [
+    {'name': 'Sapporo', 'date': 'Thu, Feb 26', 'price': '\$34.10', 'image': 'https://placeholder.com/image_sapporo.jpg'},
+    {'name': 'Osaka',     'date': 'Thu, Mar 12', 'price': '\$36.60', 'image': 'https://placeholder.com/image_osaka.jpg'},
+    {'name': 'Fukuoka',   'date': 'Sun, Mar 1',  'price': '\$43.10', 'image': 'https://placeholder.com/image_fukuoka.jpg'},
+    {'name': 'Okinawa',   'date': 'Thu, Mar 12', 'price': '\$48.30', 'image': 'https://placeholder.com/image_okinawa.jpg'},
+  ];
+
+  final List<Map<String, String>> bestDeals = [ {'name': 'Sapporo', 'date': 'Thu, Feb 26', 'price': '\$34.10', 'image': 'https://placeholder.com/image_sapporo.jpg'},
+    {'name': 'Osaka',     'date': 'Thu, Mar 12', 'price': '\$36.60', 'image': 'https://placeholder.com/image_osaka.jpg'},
+    {'name': 'Fukuoka',   'date': 'Sun, Mar 1',  'price': '\$43.10', 'image': 'https://placeholder.com/image_fukuoka.jpg'},
+    {'name': 'Okinawa',   'date': 'Thu, Mar 12', 'price': '\$48.30', 'image': 'https://placeholder.com/image_okinawa.jpg'},
+  ];
+
+  final List<Map<String, String>> trending = [ {'name': 'Sapporo', 'date': 'Thu, Feb 26', 'price': '\$34.10', 'image': 'https://placeholder.com/image_sapporo.jpg'},
+    {'name': 'Osaka',     'date': 'Thu, Mar 12', 'price': '\$36.60', 'image': 'https://placeholder.com/image_osaka.jpg'},
+    {'name': 'Fukuoka',   'date': 'Sun, Mar 1',  'price': '\$43.10', 'image': 'https://placeholder.com/image_fukuoka.jpg'},
+    {'name': 'Okinawa',   'date': 'Thu, Mar 12', 'price': '\$48.30', 'image': 'https://placeholder.com/image_okinawa.jpg'},
+  ];
+}
---
+++ lib/screens/flight_booking_middle.dart
@@ -0,0 +1,35 @@
+// lib/screens/home/flight_booking_middle.dart
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../widgets/business_class_promo.dart';
+import '../../widgets/expandable_top_section.dart';
+import '../../widgets/explore_world_section.dart';
+import '../../widgets/flight_status.dart';
+import '../../widgets/travel_inspiration_section.dart';
+import 'booking_middle_controller.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  final HomeController controller = Get.put(HomeController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ExpandableTopBar(controller: controller),
+            const BusinessClassPromo(),
+            const TravelInspirationHeader(),
+            const SizedBox(height: 8),
+            const ExploreWorldSection(),
+            const SizedBox(height: 24),
+          ],
+        ),
+      ),
+    );
+  }
+}
