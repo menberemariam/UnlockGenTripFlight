1b8042e41ac2799df25f6a1c96fea8c3c20c446d
Menberemariyam <menberemariam123@email.com>
2026-02-13 11:41:48 +0300

    Add my routes for trip app

---
+++ lib/routes/app_pages.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+import '../screen/home/flight_booking_middle.dart';
+import 'app_routes.dart';
+
+class AppPages {
+  static final routes = [
+    GetPage(
+      name: AppRoutes.HOME,
+      page: () => HomeScreen(),
+    ),
+    // Add other routes here as you create the pages
+  ];
+}
